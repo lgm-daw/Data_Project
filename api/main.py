91461454c31f335feb5b6e596fc5c83f25b17230
Lorena <lgm_daw@protonmail.com>
2026-05-17 23:18:50 +0200

    Rectificaciones en EDA

---
+++ api/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import joblib
+import pandas as pd
+
+app = FastAPI()
+
+# Cargar modelo al arrancar
+model = joblib.load("models/modelo_final.pkl")
+
+
+@app.get("/")
+def root():
+    return {"message": "API funcionando correctamente"}
+
+
+@app.post("/predict")
+def predict(data: dict):
+
+    df = pd.DataFrame([data])
+
+    prediction = model.predict(df)
+
+    return {
+        "prediction": int(prediction[0])
+    }
